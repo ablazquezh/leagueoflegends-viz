1d1082e22a7bb18f1d3a5496ca3a1260d061d5a2
Alberto Blázquez Herranz <alb.blazquez.h@gmail.com>
2020-04-10 22:52:22 +0200

    Initial commit. Load dataframes.

---
+++ app.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 10 22:26:06 2020
+
+@author: Alberto
+"""
+
+import pandas as pd
+
+df_bans = pd.read_csv('lolproject-data\\bans.csv')
+df_gold = pd.read_csv('lolproject-data\\gold.csv')
+df_kills = pd.read_csv('lolproject-data\\kills.csv')
+df_matchinfo = pd.read_csv('lolproject-data\\matchinfo.csv')
+df_monsters = pd.read_csv('lolproject-data\\monsters.csv')
+df_structures = pd.read_csv('lolproject-data\\structures.csv')
