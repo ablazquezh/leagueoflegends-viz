45e718de035dbf04bf93ecc6b84ed31c19aaf1e7
Alberto Blázquez Herranz <alb.blazquez.h@gmail.com>
2020-04-13 02:07:01 +0200

    [NOT TESTED] Finished change description scrapping for each champion.

--- patchInfoDetail_scrapper.py
+++ patchInfoDetail_scrapper.py
@@ -8,6 +8,7 @@ Created on Sat Apr 11 21:04:28 2020
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 import re
 
 month_dict = {'January': '1',
@@ -44,14 +45,14 @@ patch_info = pd.read_csv('patch_info.csv')
 
 champion_names = pd.read_csv('all_champions.csv')
 
-save_locations = ['', 'C:\\Users\\Alberto\\Desktop\\UNED\\2O CUATRIMESTRE\\VD\\data\\scrapped\\'] # put other paths if necessary
+save_locations = [''] # put other paths if necessary
 
 patch_id = []
 champion = []
 patch_date = []
 change_description = []
 
-for i in range(1):#len(patch_info)):
+for i in range(len(patch_info)):
 
     url_check = patch_info.loc[i, 'Info URL']
 
@@ -85,30 +86,126 @@ for i in range(1):#len(patch_info)):
             
             if champion_name in champion_names['Champion Name'].tolist():
                 
-                #changes = soup.findAll('ul')
-                changes =j.find_next_sibling('ul')
+                changes = j.find_next_sibling()
                 
+                #print(champion_name)
+                #print("==========")
                 
-                print(changes)
-                print("____________________")
-                """
-                for k in changes:
+                changes = changes.findAll('li')
+                info_sets = [li.find_parent().find_parent().name for li in changes]
+                info_sets_2nd = [li.find_parent().find_parent().find_parent().find_parent().name for li in changes]
+                
+                #print(info_sets)
+                #print(info_sets_2nd)
+                
+                idxs = np.where(np.isin(info_sets, 'div'))
+                idxs = list(idxs[0]) # This is done because the previous line returns an unhelping tuple
+                
+                ctr = 0
+                champion_updates_count = 0
+                for x in idxs:
                     
-                    summary = {}
-                    print(k.find('ul'))
-            """
+                    text = changes[x].get_text()
+                    text = re.sub('<img [^>]*', '', text) 
+                    text = text.replace(' >', '') # There may be a more elegant way to do this
+                    
+                    text_cells = text.split('\n')
+                    text_cells = [x for x in text_cells if len(x) > 0] # There may be a more elegant way to do this
+                    
+                    if len(text_cells) == 2: # This is, just [div, li]
+                        
+                        change_description.append({text_cells[0]: text_cells[1]})
+                        champion_updates_count = champion_updates_count +1
+                        
+                    else:
+                        
+                        if info_sets_2nd[ctr-1] != 'li': # In case everything are divs (e.g. [div, div, div, div])
+                            
+                            change_description.append({text_cells[0]: [x for idx, x in enumerate(text_cells) if idx > 0] })
+                            champion_updates_count = champion_updates_count +1
+                            
+                        else: 
+                            
+                            li_idxs = np.where(np.isin(info_sets_2nd[ctr:ctr+len(text_cells)], 'li'))
+                            
+                            li_idxs = list(li_idxs[0]) # This is done because the previous line returns an unhelping tuple
+                            
+                            if len(li_idxs) > 1:
+                            
+                                diff_li_idxs = np.diff(li_idxs) # Difference between indexes will never be > 2
+                                
+                                li_twos = np.where(np.isin(diff_li_idxs, 2))
+                                li_twos = list(li_twos[0])
+                                
+                                if len(li_twos) == 0: # All are 1s and can be coupled in the same array
+                                    
+                                    item = [text_cells[x] for x in li_idxs]
+                                    for text_idx in np.flip(np.arange(len(text_cells)-len(li_idxs))):
+                                        item = {text_cells[text_idx]: item}
+                                
+                                else:
+                                    
+                                    li_set = []
+                                    coupled = {}
+                                    for cell in np.flip(np.arange(1, len(info_sets_2nd))):
+                                    
+                                        if info_sets_2nd[cell] == 'li':
+                                        
+                                            li_set.append(text_cells[cell])
+                                        
+                                        elif info_sets_2nd[cell] == 'div':
+                                            
+                                            coupled[text_cells[cell]] = li_set
+                                            li_set = []
+                                            
+                                    change_description.append({text_cells[0]: coupled})
+                                    champion_updates_count = champion_updates_count +1
+
+                                        
+                            else: # There is only one li
+                                
+                                item = text_cells[-1]
+                                for text_idx in np.flip(np.arange(len(text_cells)-1)):
+                                    item = {text_cells[text_idx]: item}
+                                    
+                                change_description.append(item)
+                                champion_updates_count = champion_updates_count +1
+
+
+                                
+                                    
+                            #print("*********")
+                            #print(li_idxs)
+                            
+                
+                    ctr = ctr + len(text_cells)
+                                        
+                    #print(text_cells)
+
+                    #print("xxxxxxxxxx")
+                   
+                    
                 if first_added == True:
                     
                     patch_id.append(patch_id[-1])
                     patch_date.append(patch_date[-1])
                     
         
+                #print(champion_updates_count)
+                
+                initial_change_idx = len(change_description) - champion_updates_count
+                for content in change_description[initial_change_idx +1: ]:
+                    
+                    change_description[initial_change_idx].update( content )
+                    
+                del(change_description[initial_change_idx +1 : ])
+                
                 champion.append(champion_name)
                 first_added = True
 
 driver.close()
 
-patchInfo_detail = pd.DataFrame({'ID': patch_id, 'Champion': champion, 'Date': patch_date}) 
+patchInfo_detail = pd.DataFrame({'ID': patch_id, 'Champion': champion, 'Date': patch_date, 'Change Description': change_description}) 
 
 for path in save_locations:
     
