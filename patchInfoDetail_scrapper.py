43d5230861baf4c63d1faa08df5a557753fb0178
Alberto Blázquez Herranz <alb.blazquez.h@gmail.com>
2020-04-18 00:56:36 +0200

    Solved some problems related to particularities of how certain patch notes are taken.

--- patchInfoDetail_scrapper.py
+++ patchInfoDetail_scrapper.py
@@ -25,6 +25,9 @@ month_dict = {'January': '1',
  'December': '12'}
 
 
+save_locations = ['', 'C:\\Users\\Alberto\\Desktop\\UNED\\2O CUATRIMESTRE\\VD\\data\\scrapped\\'] # put other paths if necessary
+
+
 def formatString(string):
     
     string = string.replace('\n', '')
@@ -45,7 +48,6 @@ patch_info = pd.read_csv('patch_info.csv')
 
 champion_names = pd.read_csv('all_champions.csv')
 
-save_locations = [''] # put other paths if necessary
 
 patch_id = []
 champion = []
@@ -53,6 +55,9 @@ patch_date = []
 change_description = []
 
 for i in range(len(patch_info)):
+
+    #if len(champion) != len(change_description):
+     #   break
 
     url_check = patch_info.loc[i, 'Info URL']
 
@@ -81,127 +86,144 @@ for i in range(len(patch_info)):
         champion_name = j.find('span', {'style': 'white-space:normal;'})
     
         if champion_name is not None:
-    
+            
             champion_name = champion_name.get_text()
             
             if champion_name in champion_names['Champion Name'].tolist():
                 
-                changes = j.find_next_sibling()
+                if not(len(champion) > 0 and champion_name == champion[len(champion)-1]):
+                # This second condition may need to be checked if 'dl' is weidly nested as in v.5.6 Quinn's case. It will cause to have multiple dls nesting the same 'span', {'style': 'white-space:normal;'}
+    
                 
-                #print(champion_name)
-                #print("==========")
-                
-                changes = changes.findAll('li')
-                info_sets = [li.find_parent().find_parent().name for li in changes]
-                info_sets_2nd = [li.find_parent().find_parent().find_parent().find_parent().name for li in changes]
-                
-                #print(info_sets)
-                #print(info_sets_2nd)
-                
-                idxs = np.where(np.isin(info_sets, 'div'))
-                idxs = list(idxs[0]) # This is done because the previous line returns an unhelping tuple
-                
-                ctr = 0
-                champion_updates_count = 0
-                for x in idxs:
+                    changes = j.find_next_sibling()
+                    if changes == None: # May happen if 'dl' is weidly nested as in v.5.6 Quinn's case
+                        changes = j.find_next('ul')
                     
-                    text = changes[x].get_text()
-                    text = re.sub('<img [^>]*', '', text) 
-                    text = text.replace(' >', '') # There may be a more elegant way to do this
+                    #print(champion_name)
+                    #print("==========")
                     
-                    text_cells = text.split('\n')
-                    text_cells = [x for x in text_cells if len(x) > 0] # There may be a more elegant way to do this
+                    changes = changes.findAll('li')
+                    info_sets = [li.find_parent().find_parent().name for li in changes]
+                    info_sets_2nd = [li.find_parent().find_parent().find_parent().find_parent().name for li in changes]
                     
-                    if len(text_cells) == 2: # This is, just [div, li]
+                    #print(info_sets)
+                    #print(info_sets_2nd)
+                    
+                    idxs = np.where(np.isin(info_sets, 'div'))
+                    idxs = list(idxs[0]) # This is done because the previous line returns an unhelping tuple
+                    
+                    ctr = 0
+                    champion_updates_count = 0
+                    for x in idxs:
                         
-                        change_description.append({text_cells[0]: text_cells[1]})
-                        champion_updates_count = champion_updates_count +1
+                        text = changes[x].get_text()
+                        text = re.sub('<img [^>]*', '', text) 
+                        text = text.replace(' >', '') # There may be a more elegant way to do this
                         
-                    else:
+                        text_cells = text.split('\n')
+                        text_cells = [x for x in text_cells if len(x) > 0] # There may be a more elegant way to do this
                         
-                        if info_sets_2nd[ctr-1] != 'li': # In case everything are divs (e.g. [div, div, div, div])
+                        if len(text_cells) == 2: # This is, just [div, li]
                             
-                            change_description.append({text_cells[0]: [x for idx, x in enumerate(text_cells) if idx > 0] })
+                            change_description.append({text_cells[0]: text_cells[1]})
                             champion_updates_count = champion_updates_count +1
                             
-                        else: 
+                        else:
                             
-                            li_idxs = np.where(np.isin(info_sets_2nd[ctr:ctr+len(text_cells)], 'li'))
-                            
-                            li_idxs = list(li_idxs[0]) # This is done because the previous line returns an unhelping tuple
-                            
-                            if len(li_idxs) > 1:
-                            
-                                diff_li_idxs = np.diff(li_idxs) # Difference between indexes will never be > 2
+                            if info_sets_2nd[ctr-1] != 'li': # In case everything are divs (e.g. [div, div, div, div])
                                 
-                                li_twos = np.where(np.isin(diff_li_idxs, 2))
-                                li_twos = list(li_twos[0])
-                                
-                                if len(li_twos) == 0: # All are 1s and can be coupled in the same array
-                                    
-                                    item = [text_cells[x] for x in li_idxs]
-                                    for text_idx in np.flip(np.arange(len(text_cells)-len(li_idxs))):
-                                        item = {text_cells[text_idx]: item}
-                                
-                                else:
-                                    
-                                    li_set = []
-                                    coupled = {}
-                                    for cell in np.flip(np.arange(1, len(info_sets_2nd))):
-                                    
-                                        if info_sets_2nd[cell] == 'li':
-                                        
-                                            li_set.append(text_cells[cell])
-                                        
-                                        elif info_sets_2nd[cell] == 'div':
-                                            
-                                            coupled[text_cells[cell]] = li_set
-                                            li_set = []
-                                            
-                                    change_description.append({text_cells[0]: coupled})
-                                    champion_updates_count = champion_updates_count +1
-
-                                        
-                            else: # There is only one li
-                                
-                                item = text_cells[-1]
-                                for text_idx in np.flip(np.arange(len(text_cells)-1)):
-                                    item = {text_cells[text_idx]: item}
-                                    
-                                change_description.append(item)
+                                change_description.append({text_cells[0]: [x for idx, x in enumerate(text_cells) if idx > 0] })
                                 champion_updates_count = champion_updates_count +1
-
-
                                 
+                            else: 
+                                
+                                li_idxs = np.where(np.isin(info_sets_2nd[ctr:ctr+len(text_cells)], 'li'))
+                                
+                                li_idxs = list(li_idxs[0]) # This is done because the previous line returns an unhelping tuple
+                                
+                                if len(li_idxs) > 1:
+                                
+                                    diff_li_idxs = np.diff(li_idxs) # Difference between indexes will never be > 2
                                     
-                            #print("*********")
-                            #print(li_idxs)
-                            
-                
-                    ctr = ctr + len(text_cells)
+                                    li_twos = np.where(np.isin(diff_li_idxs, 2))
+                                    li_twos = list(li_twos[0])
+                                    
+                                    if len(li_twos) == 0: # All are 1s and can be coupled in the same array
                                         
-                    #print(text_cells)
-
-                    #print("xxxxxxxxxx")
-                   
+                                        item = [text_cells[x] for x in li_idxs]
+                                        for text_idx in np.flip(np.arange(len(text_cells)-len(li_idxs))):
+                                            item = {text_cells[text_idx]: item}
+                                    
+                                        change_description.append(item)
+                                        champion_updates_count = champion_updates_count +1
+                                    
+                                    else:
+                                        
+                                        li_set = []
+                                        coupled = {}
+                                        for cell in np.flip(np.arange(1, len(info_sets_2nd))):
+                                        
+                                            if info_sets_2nd[cell] == 'li':
+                                            
+                                                li_set.append(text_cells[cell])
+                                            
+                                            elif info_sets_2nd[cell] == 'div':
+                                                
+                                                coupled[text_cells[cell]] = li_set
+                                                li_set = []
+                                                
+                                        change_description.append({text_cells[0]: coupled})
+                                        champion_updates_count = champion_updates_count +1
+    
+                                            
+                                else: # There is only one li
+                                    
+                                    item = text_cells[-1]
+                                    for text_idx in np.flip(np.arange(len(text_cells)-1)):
+                                        item = {text_cells[text_idx]: item}
+                                        
+                                    change_description.append(item)
+                                    champion_updates_count = champion_updates_count +1
+    
+    
+                                    
+                                        
+                                #print("*********")
+                                #print(li_idxs)
+                                
                     
-                if first_added == True:
+                        ctr = ctr + len(text_cells)
+                                            
+                        #print(text_cells)
+    
+                        #print("xxxxxxxxxx")
+                       
+                    if first_added == True and champion_updates_count > 0: 
+                        
+                        patch_id.append(patch_id[-1])
+                        patch_date.append(patch_date[-1])
+                        
+            
+                    #print(champion_updates_count)
                     
-                    patch_id.append(patch_id[-1])
-                    patch_date.append(patch_date[-1])
+                    initial_change_idx = len(change_description) - champion_updates_count
+                    for content in change_description[initial_change_idx +1: ]:
+                        
+                        change_description[initial_change_idx].update( content )
+                        
+                    del(change_description[initial_change_idx +1 : ])
                     
-        
-                #print(champion_updates_count)
-                
-                initial_change_idx = len(change_description) - champion_updates_count
-                for content in change_description[initial_change_idx +1: ]:
+                    # [NOTE A.] May later think of a better way of handling champion_updates_count == 0 (ex. Tahm Kench - New Champion -> No changes)
+                    # Now = discard, as no change info is being given (which is what we want)
+                    if champion_updates_count > 0:
+                        champion.append(champion_name)
+                        first_added = True
+                        
+                    #elif champion_updates_count == 0:
+                    #    patch_id = patch_id[:-1]
+                    #    patch_date = patch_date[:-1]
+                        
                     
-                    change_description[initial_change_idx].update( content )
-                    
-                del(change_description[initial_change_idx +1 : ])
-                
-                champion.append(champion_name)
-                first_added = True
 
 driver.close()
 
@@ -210,3 +232,10 @@ patchInfo_detail = pd.DataFrame({'ID': patch_id, 'Champion': champion, 'Date': p
 for path in save_locations:
     
     patchInfo_detail.to_csv(path + 'patchInfo_detail.csv', index=False, sep=',')
+
+
+
+from collections import Counter
+
+Counter(patch_id).keys() # equals to list(set(words))
+Counter(patch_id).values() # counts the elements' frequency
